280b5910681c3795aa6583b079a921b067285f4d
MohamedMdaini8 <mohamed.mdaini@edu.upct.es>
2025-11-06 11:13:00 +0100

    RDA_fungi_top- and subsoil study.R

---
+++ Rscripts for Top- and Subsoil study/RDA_fungi_top- and subsoil study.R
@@ -0,0 +1,20 @@
+library(rda)
+library(correlation)
+library(corrplot)
+library(plotly)
+library(vegan)
+library(standardize)
+library(ggplot2)
+library(tidyverse)
+library(dplyr)
+
+
+a.env <- PC
+a.comm <- Fungi_genera
+pp <- disttransform(a.comm, method = 'hellinger')
+a.rda <- rda(pp~., data = a.env)
+plot(a.rda)
+ordiplot(a.rda, scaling = 2, type = "text")
+################
+anova.cca(a.rda, permutations = 999)
+anova.cca(a.rda, permutations = 999, by = "terms")
