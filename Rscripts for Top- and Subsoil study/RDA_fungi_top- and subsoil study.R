4fab7609b45aad1c432b6a1903f6c9716c52709d
MohamedMdaini8 <mohamed.mdaini@edu.upct.es>
2025-11-08 21:25:04 +0100

    RDA_fungi_top- and subsoil study.R

--- Rscripts for Top- and Subsoil study/RDA_fungi_top- and subsoil study.R
+++ Rscripts for Top- and Subsoil study/RDA_fungi_top- and subsoil study.R
@@ -8,13 +8,17 @@ library(ggplot2)
 library(tidyverse)
 library(dplyr)
 
-
 a.env <- PC
 a.comm <- Fungi_genera
-pp <- disttransform(a.comm, method = 'hellinger')
+
+# ASVs data need to be Hellinger transformed
+pp <- disttransform(a.comm, method = 'hellinger') 
 a.rda <- rda(pp~., data = a.env)
+
+#make the plot
 plot(a.rda)
 ordiplot(a.rda, scaling = 2, type = "text")
-################
+
+# significance test
 anova.cca(a.rda, permutations = 999)
 anova.cca(a.rda, permutations = 999, by = "terms")
